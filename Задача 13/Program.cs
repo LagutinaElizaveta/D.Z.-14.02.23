c6e23605ad40f28856728ce2395e06402129f542
Lagutina Elizaveta <below8585@mail.ru>
2023-02-14 18:34:12 +0300

    Домашнее задание от 14.02.23

---
+++ Задача 13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Третья цифра заданного числа или "третьей цифры нет"
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a < 100)
+{
+    Console.WriteLine("Во введенном числе нет третьей цифры");
+    return;
+}
+while (a > 999)
+{
+    a = a / 10;                                                       // a /=10;
+}
+Console.WriteLine($"Третья цифра  -  {a % 10}");
---
+++ Задача 15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Проверка дня недели
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int a = int.Parse(Console.ReadLine());
+if (a < 1 || a > 7)
+{
+    Console.WriteLine("Такого дня недели не существует. Введите корректную цифру");
+    return;
+}
+if (a >= 1 && a <= 5) Console.WriteLine("Рабочий день");
+if (a >= 6) Console.WriteLine("Выходной день");
